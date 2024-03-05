5fbecb3bdb4458cb09a392b9993370462883eb89
K.Mohammed_Ilyas <90334530+Mohammedily@users.noreply.github.com>
2024-03-05 08:54:18 +0530

    Update login.js

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -33,7 +33,7 @@ function Login() {
      e.preventDefault();
  
      try {
-         const posts = await axios.post(`https://ecommercewebbackend.vercel.app/api/signin`, 
+         const posts = await axios.post(`https://ecommerce-qrcj.onrender.com/api/signin`, 
          { email, password});
          if(posts.status === 201){
            notify()
